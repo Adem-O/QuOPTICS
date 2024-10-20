16018585de527971d6bb12f2fe9ef9953f6de479
Adem-O <138741771+Adem-O@users.noreply.github.com>
2024-10-20 18:46:43 +1100

    Update QuOpticsMainProject.py

--- QuOpticsMainProject.py
+++ QuOpticsMainProject.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
-
-
 get_ipython().run_line_magic('matplotlib', 'widget')
 
 import numpy as np
@@ -11,7 +8,8 @@ import matplotlib.pyplot as plt
 from qutip import *
 from ipywidgets import interactive, FloatSlider, IntSlider, Checkbox, VBox, HBox, Button, Output, HTML
 from IPython.display import display
-def Plot_HO():
+
+def Plot_HO(tlist = np.linspace(0, 100, 100)):
     # Initialize the figure outside the simulation function
     fig, ax = plt.subplots(figsize=(10, 8), layout='tight')
 
@@ -26,21 +24,26 @@ def Plot_HO():
 
     # Create sliders
     N_slider = IntSlider(value=30, min=1, max=100, description=r'Dim(H)')
-    n_th_a_slider = IntSlider(value=4, min=0, max=10, description=r'$\langle \hat{n}_{E}\rangle$')
+    n_th_a_slider = IntSlider(value=4, min=1, max=10, description=r'$\langle \hat{n}_{E}\rangle$')
     psi0_val_slider = IntSlider(value=9, min=0, max=30, description=r'$\rho_0$')
     kappa_slider = FloatSlider(value=0.01, min=0.0, max=0.5, step=0.01, description=r'$\gamma$')
-    Omega_slider = FloatSlider(value=0.01, min=0.0, max=0.3, step=0.01, description=r'$\Omega$')
-    k_slider = FloatSlider(value=0.05, min=0.0, max=1, step=0.01, description=r'$\kappa$')
+    Omega_slider = FloatSlider(value=0.01, min=0.0, max=0.5, step=0.01, description=r'$\Omega$')
+    k_slider = FloatSlider(value=0.05, min=0.0, max=0.2, step=0.01, description=r'$\kappa$')
 
     # Create checkboxes for driving and Kerr effects
     driving_checkbox = Checkbox(value=False, description="Include Driving")
     kerr_checkbox = Checkbox(value=False, description="Include Kerr")
+    
+    # Create checkboxes for steady state soln.
+    steady_H0_checkbox = Checkbox(value=False, description="Show steady state soln. for HO")
+    steady_H1_checkbox = Checkbox(value=False, description="Show steady state soln. for Driving")
+    steady_H2_checkbox = Checkbox(value=False, description="Show steady state soln. for Kerr")
 
     # Create the submit button
     submit_button = Button(description='Run Simulation')
 
     # Function to simulate and update plot
-    def simulate_on_submit(N=30, n_th_a=4, psi0_val=9, kappa=0.03, driving=False, kerr=False, Omega=0.0, k=0.0, base_alpha=False, driving_alpha=False, kerr_alpha=False):
+    def simulate_on_submit(N=30, n_th_a=4, psi0_val=9, kappa=0.03, driving=False, kerr=False, Omega=0.0, k=0.0, base_alpha=False, driving_alpha=False, kerr_alpha=False, steady_H0=False, steady_H1=False, steady_H2=False):
         ax.clear()  # Clear the previous plot
 
         a = destroy(N)
@@ -64,35 +67,52 @@ def Plot_HO():
         kerr_alpha = 0.3 if kerr_alpha_checkbox.value else 1.0
 
         # Simulate for base Hamiltonian (H0)
-        tlist = np.linspace(0, 100, 100)  # Shorter time range for faster updates
         opts = {'store_states': False}
         medata_base = mesolve(H0, psi0, tlist, c_op_list, [a.dag() * a], options=opts)
 
         # Plot for base Hamiltonian
-        ax.plot(tlist, medata_base.expect[0], lw=2, label=r'$\langle n(t) \rangle$ (D-H-O)', alpha=base_alpha)
+        ax.plot(tlist, medata_base.expect[0], lw=2, label=r'$\langle n(t) \rangle$ HO', alpha=base_alpha, color='tab:blue')
 
         # If driving is enabled, add the driving term and simulate
         if driving:
             H1 = Omega * (a.dag() + a)
             H_with_driving = H0 + H1
             medata_driving = mesolve(H_with_driving, psi0, tlist, c_op_list, [a.dag() * a], options=opts)
-            ax.plot(tlist, medata_driving.expect[0], lw=2, linestyle='--', label=r'$\langle n(t) \rangle$ (D-H-O w/ Driving)', alpha=driving_alpha)
+            ax.plot(tlist, medata_driving.expect[0], lw=2, linestyle='--', label=r'$\langle n(t) \rangle$ Driving', alpha=driving_alpha, color='tab:orange')
 
         # If Kerr nonlinearity is enabled, add the Kerr term and simulate
         if kerr:
             H2 = k * (a.dag() * a * a.dag() * a)
             H_with_kerr = H0 + (H1 if driving else 0) + H2
             medata_kerr = mesolve(H_with_kerr, psi0, tlist, c_op_list, [a.dag() * a], options=opts)
-            ax.plot(tlist, medata_kerr.expect[0], lw=2, linestyle=':', label=r'$\langle n(t) \rangle$ (D-D-H-O w/Kerr)', alpha=kerr_alpha)
+            ax.plot(tlist, medata_kerr.expect[0], lw=2, linestyle=':', label=r'$\langle n(t) \rangle$ Kerr', alpha=kerr_alpha, color='tab:green')
 
         # Set up the plot
         ax.set_xlabel('Time', fontsize=14)
         ax.set_ylabel(r'$\langle n \rangle$', fontsize=14)
         ax.set_title('Evolution of Photon Number Expectation Value', fontsize=16)
-
+        
+        # find steady-state solution
+        if steady_H0:
+            final_state_H0 = steadystate(H0, c_op_list)
+            fexpt_H0 = expect(a.dag() * a, final_state_H0)
+            plt.axhline(y=fexpt_H0, linestyle=':', lw=1.5, label='Steady H1', color='tab:blue')
+        if steady_H1 and driving:
+            H1 = Omega * (a.dag() + a)
+            final_state_H0_1 = steadystate(H0+H1, c_op_list)
+            fexpt_H0_1 = expect(a.dag() * a, final_state_H0_1)
+            plt.axhline(y=fexpt_H0_1, linestyle=':', lw=1.5, label='Steady H2', color='tab:orange')
+        
+        if steady_H2 and kerr:
+            H2 = k * (a.dag() * a * a.dag() * a)
+            final_state_H0_1_2 = steadystate(H0 + (H1 if driving else 0) + H2, c_op_list)
+            fexpt_H0_1_2 = expect(a.dag() * a, final_state_H0_1_2)
+            plt.axhline(y=fexpt_H0_1_2, linestyle=(2,(2,2)), lw=1.5, label='Steady H3', color='tab:green')
+            
         # Move the legend to the right side
         ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-
+        ax.set_ylim(0,15)
+        
         # Redraw the updated plot
         plt.draw()
 
@@ -103,7 +123,8 @@ def Plot_HO():
         simulate_on_submit(N_slider.value, n_th_a_slider.value, psi0_val_slider.value, 
                            kappa_slider.value, driving_checkbox.value, kerr_checkbox.value, 
                            Omega_slider.value, k_slider.value, 
-                           base_alpha_checkbox.value, driving_alpha_checkbox.value, kerr_alpha_checkbox.value)
+                           base_alpha_checkbox.value, driving_alpha_checkbox.value, kerr_alpha_checkbox.value,
+                           steady_H0_checkbox.value,steady_H1_checkbox.value,steady_H2_checkbox.value)
         loading_label.layout.display = 'none'  # Hide loading label
         submit_button.disabled = False  # Re-enable the submit button
 
@@ -111,10 +132,13 @@ def Plot_HO():
     submit_button.on_click(on_submit)
 
     # Arrange widgets in a box, include the transparency checkboxes
-    controls_box = VBox([N_slider, n_th_a_slider, psi0_val_slider, kappa_slider, 
+    controls_box = VBox([HBox([N_slider, n_th_a_slider]),
+                         HBox([psi0_val_slider, kappa_slider]), 
                          HBox([driving_checkbox, Omega_slider]), 
                          HBox([kerr_checkbox, k_slider]), 
-                         base_alpha_checkbox, driving_alpha_checkbox, kerr_alpha_checkbox, 
+                         HBox([base_alpha_checkbox, steady_H0_checkbox]),
+                         HBox([driving_alpha_checkbox,steady_H1_checkbox]),
+                         HBox([kerr_alpha_checkbox,steady_H2_checkbox]), 
                          submit_button, loading_label])  # Include the loading label in controls
 
     # Display controls
@@ -124,7 +148,8 @@ def Plot_HO():
     simulate_on_submit(N_slider.value, n_th_a_slider.value, psi0_val_slider.value, 
                        kappa_slider.value, driving_checkbox.value, kerr_checkbox.value, 
                        Omega_slider.value, k_slider.value, 
-                       base_alpha_checkbox.value, driving_alpha_checkbox.value, kerr_alpha_checkbox.value)
+                       base_alpha_checkbox.value, driving_alpha_checkbox.value, kerr_alpha_checkbox.value,
+                       steady_H0_checkbox.value,steady_H1_checkbox.value,steady_H2_checkbox.value)
 
 
 # In[ ]:
